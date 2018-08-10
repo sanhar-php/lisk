78329e6771bf2c59c4c1d0a950ff629d66967ee9
Maciej Baj <macie.baj@gmail.com>
2018-08-10 16:43:35 +0200

    Remove banned checks before peers upsert

--- logic/peers.js
+++ logic/peers.js
@@ -167,16 +167,6 @@ Peers.prototype.upsert = function(peer, insertOnly) {
 			const recentPeer = self.peersManager.getByAddress(peer.string);
 			// If an entry exists in Ban Manager don't allow to change the state during updates.
 			// Prevents intentionally disconnections of temporarily banned peers.
-			if (
-				self.banManager.bannedPeers[peer.string] &&
-				peer.state !== Peer.STATE.BANNED
-			) {
-				library.logger.info('Attempt to unban peer rejected', peer.string);
-				library.logger.debug('Attempt to unban peer rejected', {
-					peer: peer.object(),
-				});
-				return failureCodes.ON_MASTER.UPDATE.BANNED;
-			}
 			update(recentPeer, peer);
 		} else {
 			return failureCodes.ON_MASTER.INSERT.INSERT_ONLY_FAILURE;
